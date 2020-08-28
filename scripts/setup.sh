c4dddbe16feaea73955643005407b954f3ca0054
Christian Mahnke <cmahnke@gmail.com>
2020-08-28 19:14:50 +0200

    Remove build error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,7 @@ WD=`pwd`
 # IIFF
 for IMAGE in `ls -1 content/post/**/page*.jpg content/post/**/front.jpg`
 do
+    echo "Generating IIIF files for $IMAGE"
 #    iiif_static.py -d `dirname $IMAGE` $IMAGE
 done
 

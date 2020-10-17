7e9d458232745d3f6ea6a8452eee4cbf07db4930
Christian Mahnke <cmahnke@gmail.com>
2020-10-17 08:20:55 +0200

    Switch to jsBuild and SCSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 window.$ = window.jQuery = require('jquery');
+
+require('bootstrap');
+require('headroom.js');
+//require('jQuery.headroom');
+require('masonry-layout');
+require('slick-carousel');
+
 require('./iiif-viewer');

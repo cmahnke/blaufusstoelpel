c119a596420d7c7d73bd88aa1606fecf811753bb
Christian Mahnke <cmahnke@gmail.com>
2020-11-03 12:20:00 +0100

    Basic animations work

--- assets/js/iiif-image-viewer.js
+++ assets/js/iiif-image-viewer.js
@@ -1,4 +1,4 @@
-/* Inspired from https://openlayers.org/en/latest/examples/iiif.html */
+/* Inspired from https://openlayers.org/en/latest/examples/iiif.html and several others  */
 
 import IIIF from 'ol/source/IIIF';
 import IIIFInfo from 'ol/format/IIIFInfo';
@@ -6,21 +6,39 @@ import Map from 'ol/Map';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import {Control, FullScreen, Rotate, Zoom} from 'ol/control';
-
-/* TODO finish this
-*/
+import { fromUserExtent } from 'ol/proj.js';
 
 var AnimatedView = /*@__PURE__*/ (function(View) {
 
     function AnimatedView(opt_options) {
         View.call(this, opt_options);
         this.pauseableAnimations_ = [];
+        this.animationsPointer_ = 0;
         this.lastAnimation_ = {};
     }
 
     if (View) AnimatedView.__proto__ = View;
     AnimatedView.prototype = Object.create(View && View.prototype);
     AnimatedView.prototype.constructor = AnimatedView;
+
+    // Emulate a ring data structure
+    AnimatedView.prototype.getPauseableAnimation_ = function () {
+        if (this.pauseableAnimations_.length < this.animationsPointer_ + 1) {
+            this.animationsPointer_++;
+            return this.pauseableAnimations_[this.animationsPointer_]
+        } else if (this.pauseableAnimations_.length == this.animationsPointer_ + 1) {
+            this.animationsPointer_ = 0;
+            return this.pauseableAnimations_[this.animationsPointer_];
+        }
+    }
+
+    // This is the callback, when one animation has finished
+    AnimatedView.prototype.nextAnimation_ = function (completed) {
+        var context = this;
+        if (completed === undefined || completed) {
+            this.animate(this.getPauseableAnimation_(), function (state) {context.nextAnimation_(state)});
+        }
+    }
 
     AnimatedView.prototype.pauseAnimation = function () {
         if (!this.getAnimating()) {
@@ -31,8 +49,9 @@ var AnimatedView = /*@__PURE__*/ (function(View) {
         var now = Date.now();
         var elapsed = now - animation.start;
         let stopState = {
-            center: this.getCenter(),
-            zoom: this.getZoom(),
+            center: animation.center,
+            zoom: animation.zoom,
+            rotation: animation.rotation,
             duration: animation.duration - elapsed
         };
         this.lastAnimation_ = stopState;
@@ -44,34 +63,56 @@ var AnimatedView = /*@__PURE__*/ (function(View) {
         if (this.getAnimating()) {
           return;
         }
-        // Check if we stopped
-        var startAnimation;
-/*
-        if () {
-
-        } else {
-
+        // Check if we stopped, if yes, reset
+        if (Object.keys(this.lastAnimation_).length !== 0) {
+            this.lastAnimation_ = {};
         }
-*/
-        // Get first animation
-
-
-        // Pass it to animation method and add a callback
-
-
+        // Get first animation,pass it to animation method and add a callback
+        var context = this;
+        this.animate(this.getPauseableAnimation_, function (state) {context.nextAnimation_(state)});
     }
 
     AnimatedView.prototype.resumeAnimation = function () {
-        this.startAnimation_();
+        if (this.getAnimating()) {
+          return;
+        }
+        // Check if we stopped
+        if (Object.keys(this.lastAnimation_).length === 0) {
+            this.startAnimation_();
+        } else {
+            // Start stopped animation
+            var context = this;
+            this.animate(this.lastAnimation_, function (state) {context.nextAnimation_(state)});
+        }
     }
 
-    AnimatedView.prototype.pauseableAnimation = function (var_args) {
+    AnimatedView.prototype.setPauseableAnimation = function (var_args) {
         var args = new Array(arguments.length);
+        for (let i = 0; i < args.length; ++i) {
+          var options = arguments[i];
+          /*
+          if (!'center' in options) {
+              options.center = this.getCenter();
+          }
+          */
+          args[i] = options;
+        }
         this.pauseableAnimations_ = args;
-        this.startAnimation_();
     }
 
+    AnimatedView.prototype.getPauseableAnimation = function () {
+        return this.pauseableAnimations_;
+    }
 
+    //Even though the OL API is quite good there are some beginners mistakes, like missing symetry
+    AnimatedView.prototype.setResolutions = function (resolutions) {
+        this.resolutions_ = resolutions;
+    }
+    AnimatedView.prototype.setExtent = function (extent) {
+        var options = {};
+        options.extent = fromUserExtent(extent, this.projection_);
+        this.applyOptions_(options);
+    }
 
     return AnimatedView;
 }(View));
@@ -210,7 +251,7 @@ postsaison_4_87page048.getView().animate({center:[1000,-3000], duration: 3000 zo
 postsaison_4_87page048.getView().animate({center:[1000,-3000], duration: 3000, zoom: 1})
 */
 
-window.animatedMap = function(element, url, rotation, baseURL, initialZoom, animation) {
+window.animatedMap = function(element, url, rotation, baseURL, initialZoom, animation, moElement) {
     var initialRotation = 0;
     if (rotation !== undefined && rotation != 0) {
         initialRotation = rotation * Math.PI / 180;
@@ -223,7 +264,12 @@ window.animatedMap = function(element, url, rotation, baseURL, initialZoom, anim
             controls: [],
             layers: [layer],
             target: element,
-        });
+        }),
+        view = new AnimatedView({
+            constrainOnlyCenter: true,
+            rotation: initialRotation
+        })
+        ;
 
     fetch(url)
         .then(function(response) {
@@ -241,15 +287,21 @@ window.animatedMap = function(element, url, rotation, baseURL, initialZoom, anim
                     }
                     var iiifTileSource = new IIIF(options);
                     layer.setSource(iiifTileSource);
-                    map.setView(
-                        new AnimatedView({
-                            resolutions: iiifTileSource.getTileGrid().getResolutions(),
-                            extent: iiifTileSource.getTileGrid().getExtent(),
-                            constrainOnlyCenter: true,
-                            rotation: initialRotation
-                        })
-                    );
+                    view.setExtent(iiifTileSource.getTileGrid().getExtent());
+                    view.setResolutions(iiifTileSource.getTileGrid().getResolutions());
+                    /*
+                    var view = new AnimatedView({
+                        resolutions: iiifTileSource.getTileGrid().getResolutions(),
+                        //extent: iiifTileSource.getTileGrid().getExtent(),
+                        constrainOnlyCenter: true,
+                        rotation: initialRotation
+                    });
+                    */
+                    map.setView(view);
                     map.getView().fit(iiifTileSource.getTileGrid().getExtent());
+                    if (initialZoom !== undefined && initialZoom !== '') {
+                        map.getView().setZoom(initialZoom);
+                    }
                 })
                 .catch(function(body) {
                     console.log('Could not read image info json. ' + body);
@@ -259,21 +311,17 @@ window.animatedMap = function(element, url, rotation, baseURL, initialZoom, anim
             console.log('Could not read data from URL.');
         });
 
-        if (initialZoom !== undefined && initialZoom !== '') {
-            map.getView().setZoom(initialZoom);
-        }
 
-        if (animation !== undefined && animation !== '') {
+
+        if (animation !== undefined && animation !== '' && moElement !== undefined) {
+            view.setPauseableAnimation(animation);
             map.once('rendercomplete', function() {
-                map.on('mouseenter', function () {
-                    console.log('Starting animation');
-                    map.getView().animate(animation);
-
+               moElement.addEventListener('mouseenter', function () {
+                    view.resumeAnimation();
                 });
-                map.on('mouseout', function () {
-                    map.cancelAnimations();
+                moElement.addEventListener('mouseleave', function (event) {
+                    view.pauseAnimation();
                 });
-
             });
         }
 

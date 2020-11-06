7cf12f10cd9e52c6128c72489a35782c7d9ff190
Christian Mahnke <cmahnke@gmail.com>
2020-11-06 13:25:03 +0100

    Fix favicon

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,6 @@ if [[ -z "$SKIP_IIIF" ]] ; then
     ./scripts/iiif.sh
 fi
 
-#NPM dependencies
-yarn install
-yarn run svgo
-
 # Favicons
 # See https://gist.github.com/pfig/1808188
 convert static/images/blaufusstoelpel.svg static/images/blaufusstoelpel.png
@@ -19,3 +15,7 @@ convert static/images/favicon-128.png -resize 16x16 static/images/favicon-16.png
 convert static/images/favicon-128.png -resize 32x32 static/images/favicon-32.png
 convert static/images/favicon-128.png -resize 64x64 static/images/favicon-64.png
 convert static/images/favicon-16.png static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png -colors 256 static/images/favicon.ico
+
+#NPM dependencies
+yarn install
+yarn run svgo

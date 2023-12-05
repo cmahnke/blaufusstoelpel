65fb1854a8ba12465f1d8adffe566377eae37667
Christian Mahnke <cmahnke@gmail.com>
2023-12-05 21:53:16 +0100

    disable JS changes during build

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -34,7 +34,7 @@ echo "OS is '$OS', sed is '$SED'"
 $SED -i -E 's/@import url\("https:\/\/fonts\.googleapis\.com.*//g' themes/parsa/assets/css/style.css
 #find node_modules/vis-timeline/standalone/ -type f -name '*.js' -exec $SED -i -E 's/\/\*\s*@__PURE__\s*\*\///g' '{}' \;
 #find node_modules/vis-timeline/standalone/ -type f -name '*.js' -exec $SED -i -E 's/_classCallCheck/classCallCheck/g' '{}' \;
-find node_modules/vis-timeline/standalone/ -type f -name '*.js' -exec $SED -i -E 's/_classCallCheck.*//g' \;
+#find node_modules/vis-timeline/standalone/ -type f -name '*.js' -exec $SED -i -E 's/_classCallCheck.*//g' \;
 
 yarn run svgo
 
